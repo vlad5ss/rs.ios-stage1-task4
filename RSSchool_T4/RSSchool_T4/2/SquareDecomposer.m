0bacd006a42d8e4249b43e43135ad66fca27b66f
vlad5ss <vlad5grup@gmail.com>
2020-04-13 14:16:48 +0400

    add:stage4

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -3,6 +3,25 @@
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
     // good luck
-    return @[@0];
+    // good luck
+    if (!number || ![number integerValue]) { return nil; }
+    NSArray *itsNumber = [self
+                          decomposer:[number integerValue]
+                          stay:[number integerValue] * [number integerValue]
+                          ];
+    return [itsNumber subarrayWithRange:NSMakeRange(0, [itsNumber count] - 1)];
 }
+
+- (NSArray<NSNumber *> *)decomposer:(NSInteger)n stay:(NSInteger)stay2 {
+    if (!stay2) { return @[@(n)]; }
+    
+    for (NSInteger i = n - 1; i > 0; i--) {
+        if ((stay2 - i * i) < 0) { continue; };
+        NSArray *result = [self decomposer:i stay:stay2 - i * i];
+        if (result) { return [result arrayByAddingObject:@(n)]; }
+    }
+    
+    return nil;
+}
+
 @end
--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -2,7 +2,32 @@
 
 @implementation TelephoneFinder
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
-    // good luck
-    return @[@"0"];
+    NSDictionary *mydictionary = @{
+                                   @"0": @[@"8"],
+                                   @"1": @[@"2", @"4"],
+                                   @"2": @[@"1", @"3", @"5"],
+                                   @"3": @[@"2", @"6"],
+                                   @"4": @[@"1", @"5", @"7"],
+                                   @"5": @[@"2", @"4", @"6", @"8"],
+                                   @"6": @[@"3", @"5", @"9"],
+                                   @"7": @[@"4", @"8"],
+                                   @"8": @[@"5", @"7", @"9", @"0"],
+                                   @"9": @[@"6", @"8"],
+                                   };
+    int val = [number intValue];
+    if (val < 0) {
+        return nil;
+    }
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (int i = 0 ; i < number.length; i++){
+        NSRange range = NSMakeRange(i, 1);
+        NSArray * dictionaryRefactor = [mydictionary valueForKey:[number substringWithRange:range]];
+        for (int j = 0; j < dictionaryRefactor.count; j++){
+            NSString * newDictionary = [number stringByReplacingCharactersInRange:range withString:dictionaryRefactor[j]];
+            [result addObject:newDictionary];
+        }
+    }
+    return result;
 }
+
 @end
